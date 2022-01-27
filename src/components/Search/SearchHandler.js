da0f15a32d2c9be19db7c1cf8c2e8b33e4d53de0
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-01-27 20:40:20 +0000

    fixed ctrl k detection for the search component

--- src/components/Search/SearchHandler.js
+++ src/components/Search/SearchHandler.js
@@ -2,13 +2,21 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Search from "./"
 import { useSearchState, setSearchState } from "../../features/NavBarSlice"
+import { useOs } from "@mantine/hooks"
 import PropTypes from "prop-types"
 
 export function SearchHandler({ children }) {
   const showSearch = useSelector(useSearchState)
   const dispatch = useDispatch()
-  const onKeyDown = e => {
-    if ((e.metaKey && e.key === "k") || (e.metaKey && e.key === "K")) {
+  const os = useOs()
+
+  function onKeyDown(e) {
+    if (
+      (os === "macos" ? e.metaKey : e.ctrlKey && e.key === "k") ||
+      (os === "macos" ? e.metaKey : e.ctrlKey && e.key === "K")
+    ) {
+      console.log(e)
+      e.preventDefault()
       dispatch(setSearchState(true))
     } else if (e.key === "Escape") {
       dispatch(setSearchState(false))
